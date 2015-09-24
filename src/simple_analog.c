f1561541fb55e69e8db4cc15738a428b7182c347
Nomura, Yoshihide <nomura@pobox.com>
2015-09-25 00:57:24 +0900

    changed date and week to avoid overwrap with hands

--- src/simple_analog.c
+++ src/simple_analog.c
@@ -2,7 +2,7 @@
 
 #include <pebble.h>
 
-#define NUM_Y 40
+#define NUM_Y 25
 #define DAY_Y 90
 
 static Window *window;
@@ -12,6 +12,7 @@ static BitmapLayer *back_layer;
 
 static GPath *s_minute_arrow, *s_hour_arrow;
 static char s_num_buffer[4], s_day_buffer[6];
+static int hand_layout;
 
 static void hands_update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
@@ -39,24 +40,32 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   gpath_draw_filled(ctx, s_minute_arrow);
   gpath_draw_outline(ctx, s_minute_arrow);
 
-  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
+  int32_t hour_angle = (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6);
+  gpath_rotate_to(s_hour_arrow, hour_angle);
   gpath_draw_filled(ctx, s_hour_arrow);
   gpath_draw_outline(ctx, s_hour_arrow);
+
+  hand_layout = 1 << (int)(minute_angle * 4 / TRIG_MAX_ANGLE);
+  hand_layout |= 1 << (int)(hour_angle * 4 / TRIG_MAX_ANGLE);
+//  APP_LOG(APP_LOG_LEVEL_DEBUG, "hand layout %d", hand_layout);
 
   // second hand
 #ifdef PBL_COLOR
   graphics_context_set_stroke_color(ctx, GColorRed);
+  graphics_context_set_fill_color(ctx, GColorRed);
 #else
   graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_context_set_fill_color(ctx, GColorWhite);
 #endif
-  graphics_draw_line(ctx, second_hand, center);
+//  graphics_draw_line(ctx, second_hand, center);
+  graphics_fill_circle(ctx, second_hand, 3);
 
   // dot in the middle
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
 }
 
-static void draw_outline_text(GContext *ctx, const char* text, const GFont font, const GRect rect, GColor forecolor, GColor outlinecolor) {
+void draw_outline_text(GContext *ctx, const char* text, const GFont font, const GRect rect, GColor forecolor, GColor outlinecolor) {
   graphics_context_set_text_color(ctx, outlinecolor);
   graphics_draw_text(ctx, text, font, GRect(rect.origin.x+1, rect.origin.y+1, rect.size.w, rect.size.h), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
   graphics_draw_text(ctx, text, font, GRect(rect.origin.x+1, rect.origin.y-1, rect.size.w, rect.size.h), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
@@ -70,25 +79,28 @@ static void date_update_proc(Layer *layer, GContext *ctx) {
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
 
-  strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
-  GFont font =  fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT);
+  // date
+  strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
+  GPoint date_point = TEXT_POINTS[hand_layout][0];
+  GFont font = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
 #ifdef PBL_COLOR
-  if (t->tm_wday == 0 || t->tm_wday == 6) {
-    draw_outline_text(ctx, s_day_buffer, font, GRect(10, DAY_Y, 120, 45), GColorRed, GColorBulgarianRose);
-  } else {
-    draw_outline_text(ctx, s_day_buffer, font, GRect(10, DAY_Y, 120, 45), GColorGreen, GColorArmyGreen);
-  }
+  draw_outline_text(ctx, s_num_buffer, font, GRect(date_point.x, date_point.y, 40, 30), GColorGreen, GColorArmyGreen);
 #else
-  draw_outline_text(ctx, s_day_buffer, font, GRect(10, DAY_Y, 120, 45), GColorWhite, GColorBlack);
+  draw_outline_text(ctx, s_num_buffer, font, GRect(date_point.x, date_point.y, 40, 30), GColorWhite, GColorBlack);
 #endif
 
-  strftime(s_num_buffer, sizeof(s_num_buffer), "%d", t);
+  // week
+  strftime(s_day_buffer, sizeof(s_day_buffer), "%a", t);
+  GPoint week_point = TEXT_POINTS[hand_layout][1];
+  font =  fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
 #ifdef PBL_COLOR
-  font = fonts_get_system_font(FONT_KEY_LECO_28_LIGHT_NUMBERS);
-  draw_outline_text(ctx, s_num_buffer, font, GRect(10, NUM_Y, 120, 32), GColorGreen, GColorArmyGreen);
+  if (t->tm_wday == 0 || t->tm_wday == 6) {
+    draw_outline_text(ctx, s_day_buffer, font, GRect(week_point.x, week_point.y, 40, 30), GColorRed, GColorBulgarianRose);
+  } else {
+    draw_outline_text(ctx, s_day_buffer, font, GRect(week_point.x, week_point.y, 40, 30), GColorGreen, GColorArmyGreen);
+  }
 #else
-  font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
-  draw_outline_text(ctx, s_num_buffer, font, GRect(10, NUM_Y, 120, 32), GColorWhite, GColorBlack);
+  draw_outline_text(ctx, s_day_buffer, font, GRect(week_point.x, week_point.y, 40, 30), GColorWhite, GColorBlack);
 #endif
 }
 
@@ -100,7 +112,7 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  back_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MODERN_BACKGROUND);
+  back_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BG_LOGO);
   back_layer = bitmap_layer_create(bounds);
   bitmap_layer_set_bitmap(back_layer, back_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(back_layer));
@@ -137,6 +149,7 @@ static void init() {
 
   s_day_buffer[0] = '\0';
   s_num_buffer[0] = '\0';
+  hand_layout = 0;
 
   // init hand paths
   s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
